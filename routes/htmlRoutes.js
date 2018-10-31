eeed06c5b60e0f5d29ca734f5547ae231e28adb1
shashabazaar <shafiqah.sharif@yahoo.com>
2018-10-31 13:11:36 -0400

    updated files and routes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,89 +1,31 @@
-var db = require("../models");
-const path = require("path");
-const sql = require("sequelize");
+// Dependencies
+// =============================================================
+var path = require("path");
 
+// Routes
+// =============================================================
 module.exports = function(app) {
-  // Load index page
+  // Each of the below routes just handles the HTML page that the user gets sent to.
+  // index route loads main.html
   app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.render("index", {
-        msg: "Welcome!",
-        examples: dbExamples
-      });
-    });
+    res.sendFile(path.join(__dirname, "../public/main.html"));
   });
 
-  // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({
-      where: {
-        id: req.params.id
-      }
-    }).then(function(dbExample) {
-      res.render("example", {
-        example: dbExample
-      });
-    });
+  app.get("/stats", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/stats.html"));
   });
 
-
-
-  // // To load the stats handlebars page for stats
-  app.get("/stats", function(req, res) {
-
-    db.find(... = ...)
-
-    db.Post.findAll({
-      where: {
-        category: req.params.category
-      }
-    })
-
-    //call ID
-
-//the information we have to identify the correct answers for questions in a quiz are the primary key id for the question in said quiz in the db questions table and the string correct or incorrect depending on the answer
-
-//make a if stmt for whether the
-
-
-    //answer corect is a boolean, then check how many "true" for each 
-    //query user info
-    //then do math for this
-
-
-    // res.render("stats", {
-    //   quizOneProgress: 60
-    // });
-
-  // //for updating the progress bar with handlebars
-  // $(".progress-bar").each(function(){
-  //   console.log($(this).attr("aria-valuenow"));
-   
-  //       //for each quiz id
-  //       //for each question id
-  //       //if question correct then add % 1/?? total of question.length
-  
-  // });
-  
-  // });
-
-  //old code to render stats page
-  app.get("/stats", function(req, res) {
-    res.render("stats");
-
-
-
-    //get to the quiz page
+  // login route loads login.html
+  app.get("/login", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/login.html"));
+  });
   app.get("/quiz/:quizId", (req, res) => {
-    res.render("questions", {
+    res.render("questions.handlebars", {
       quiz: req.params.quizId
     });
   });
 
-  // Render 404 page for any unmatched routes
-  app.get("*", function(req, res) {
-    console.log("hit the star route");
-    res.render("404");
-  });
+  app.use(function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/404.html"));
   });
 };
