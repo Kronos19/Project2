02ba11145bc70ca11993da4bd69222aa07bf2059
Anant Sharma <anantsh7@gmail.com>
2018-10-31 13:26:43 -0400

    crypt removed

--- models/users.js
+++ models/users.js
@@ -28,14 +28,5 @@ module.exports = function(sequelize, DataTypes) {
     }
     
   });
-    // Creating a custom method for our User model. This will check if an unhashed password entered by the user can be compared to the hashed password stored in our database
-    Users.prototype.validPassword = function (password) {
-      return bcrypt.compareSync(password, this.password);
-    };
-    // Hooks are automatic methods that run during various phases of the User Model lifecycle
-    // In this case, before a User is created, we will automatically hash their password
-    Users.hook("beforeCreate", function (user) {
-      user.password = bcrypt.hashSync(user.password, bcrypt.genSaltSync(10), null);
-    });
     return Users;
 }
